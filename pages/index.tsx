366aee9867b0da5fda338e68fe0475b737a3d472
lamhoang1256 <lamhoang1256@gmail.com>
2022-07-07 20:21:50 +0700

    Chore: testing deloy vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,39 @@
-import { getHomeData } from "utils/api";
-import { Layout } from "components/layouts";
-import { HomeFeature, HomeNewest } from "modules/home";
-import { INewestComics, ITopComics } from "interfaces/home";
+// import { getHomeData } from "utils/api";
+// import { Layout } from "components/layouts";
+// import { HomeFeature, HomeNewest } from "modules/home";
+// import { INewestComics, ITopComics } from "interfaces/home";
 
-interface HomeProps {
-  data: {
-    topComics: ITopComics;
-    newestComics: INewestComics;
-  };
-}
+// interface HomeProps {
+//   data: {
+//     topComics: ITopComics;
+//     newestComics: INewestComics;
+//   };
+// }
 
-const Home = ({ data }: HomeProps) => {
-  return (
-    <Layout title="HomePage">
-      <div className="layout-container">
-        <HomeFeature topComics={data.topComics}></HomeFeature>
-        <HomeNewest data={data.newestComics}></HomeNewest>
-      </div>
-    </Layout>
-  );
+// const Home = ({ data }: HomeProps) => {
+//   return (
+//     <Layout title="HomePage">
+//       <div className="layout-container">
+//         <HomeFeature topComics={data.topComics}></HomeFeature>
+//         <HomeNewest data={data.newestComics}></HomeNewest>
+//       </div>
+//     </Layout>
+//   );
+// };
+
+// export async function getStaticProps() {
+//   const { data } = await getHomeData();
+//   return {
+//     props: { data },
+//   };
+// }
+
+// export default Home;
+
+import React from "react";
+
+const Home = () => {
+  return <div>Home</div>;
 };
-
-export async function getStaticProps() {
-  const { data } = await getHomeData();
-  return {
-    props: { data },
-  };
-}
 
 export default Home;
