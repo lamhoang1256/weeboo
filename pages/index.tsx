37e81540a0fbdf6ed53df0fc4ec13c735e2c777a
lamhoang1256 <lamhoang1256@gmail.com>
2022-07-07 18:12:27 +0700

    Chore: temp comment page home to upload to vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,38 @@
-import { getHomeData } from "utils/api";
-import { Layout } from "components/layouts";
-import HomeBanner from "modules/home/HomeBanner";
-import { ILastestComics, ITopComics } from "interfaces/home";
+// import { getHomeData } from "utils/api";
+// import { Layout } from "components/layouts";
+// import HomeBanner from "modules/home/HomeBanner";
+// import { ILastestComics, ITopComics } from "interfaces/home";
 
-interface HomeProps {
-  data: {
-    topComics: ITopComics;
-    lastestComics: ILastestComics;
-  };
-}
+// interface HomeProps {
+//   data: {
+//     topComics: ITopComics;
+//     lastestComics: ILastestComics;
+//   };
+// }
 
-const Home = ({ data }: HomeProps) => {
-  return (
-    <Layout title="HomePage">
-      <div className="layout-container">
-        <HomeBanner topComics={data.topComics}></HomeBanner>
-      </div>
-    </Layout>
-  );
+// const Home = ({ data }: HomeProps) => {
+//   return (
+//     <Layout title="HomePage">
+//       <div className="layout-container">
+//         <HomeBanner topComics={data.topComics}></HomeBanner>
+//       </div>
+//     </Layout>
+//   );
+// };
+
+// export async function getStaticProps() {
+//   const { data } = await getHomeData();
+//   return {
+//     props: { data },
+//   };
+// }
+
+// export default Home;
+
+import React from "react";
+
+const index = () => {
+  return <div>index</div>;
 };
 
-export async function getStaticProps() {
-  const { data } = await getHomeData();
-  return {
-    props: { data },
-  };
-}
-
-export default Home;
+export default index;
